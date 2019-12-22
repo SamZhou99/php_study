1fec751d3856c812c9288838ac2253918e966c08
sam.z <45636987@qq.com>
2019-12-23 07:56:07 +0800

    update

--- php5.cn/template/cache_tpl/fba59e5c347475526b4467651886d830_tpl.php
+++ php5.cn/template/cache_tpl/fba59e5c347475526b4467651886d830_tpl.php
@@ -1,10 +1,10 @@
-<!DOCTYPE html>
+<?php defined('PHPNEW_STATIC_TPL') === false && exit('PHPnew CACHE_TPL 7.3.1');?><?php extract($this->templates_assign);?><!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0">
 	<meta charset="utf-8">
 	<title>LINKS</title>
-	<style>
+	<style type="text/css">
 		body{
 			text-align: center;
 			background-color: #333333;
@@ -93,10 +93,10 @@
 <ul>
 <?php foreach ($category as $categoryItem) : ?>
 <li>
-<div class="item-title">{$categoryItem['name']}</div>
+<div class="item-title"><?php echo $categoryItem['name']?></div>
 <div class="item-link">
 	<?php foreach ($categoryItem['data'] as $linkItem) : $url = $linkItem['url']; $name = $linkItem['name'];?>
-	<a href="{$url}" target="_blank">{$name}</a>
+	<a href="<?php echo $url?>" target="_blank"><?php echo $name?></a>
 	<?php endforeach; ?>
 </div>
 </li>
