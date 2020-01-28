d4dedc8c9145aad00e2a307218734efe3a52a4f9
sam.z <45636987@qq.com>
2020-01-28 21:35:57 +0800

    update

--- php3.cn/edit.php
+++ php3.cn/edit.php
@@ -6,34 +6,50 @@ include('./class/Config.class.php');
 $Config = new Config();
 $mysql = new PHPMysql($Config::$DB);
 
-function location()
+function location($url = '/')
 {
     Header("HTTP/1.1 303 See Other");
-    Header("Location: /");
+    Header("Location: $url");
     exit();
+}
+
+function writeHtml()
+{
+    $html = file_get_contents('http://php3.cn/index.php');
+    $fp = fopen('./index.html', 'w');
+    fwrite($fp, $html);
+    fclose($fp);
 }
 
 if ($_SESSION['isAdmin'] !== 9999) {
     location();
 }
 
+// 编辑表单
 $act = isset($_POST['act']) ? $_POST['act'] : '';
 $type = isset($_POST['type']) ? $_POST['type'] : '';
 if ($act === 'edit') {
+    $id = isset($_POST['id']) ? (int) $_POST['id'] : 0;
+    $name = isset($_POST['name']) ? $_POST['name'] : '';
+    $sort = isset($_POST['sort']) ? (int) $_POST['sort'] : 0;
+    $status = isset($_POST['status']) ? (int) $_POST['status'] : 0;
     if ($type === 'category') {
-        // 
-        $id = isset($_POST['id']) ? (int) $_POST['id'] : 0;
-        $name = isset($_POST['name']) ? $_POST['name'] : '';
-        $sort = isset($_POST['sort']) ? (int) $_POST['sort'] : 0;
-        $s = $mysql->where(array('id' => $id))->update('category', array('name' => $name, 'sort' => $sort));
+        // 分类 数据更新
+        $s = $mysql->where(array('id' => $id))->update('category', array('name' => $name, 'sort' => $sort, 'status' => $status));
     }
     if ($_POST['type'] === 'links') {
-        //
+        // 链接 数据更新
+        $url = isset($_POST['url']) ? $_POST['url'] : '';
+        $hot = isset($_POST['hot']) ? (int) $_POST['hot'] : 0;
+        $s = $mysql->where(array('id' => $id))->update('links', array('name' => $name, 'sort' => $sort, 'url' => $url, 'hot' => $hot, 'status' => $status));
     }
-    location();
+    writeHtml();
+    location('/index.php');
     exit("$id, $name, $sort");
 }
 
+
+// 查询表单
 $type = isset($_GET['type']) ? $_GET['type'] : '';
 $id = isset($_GET['id']) ? (int) $_GET['id'] : 0;
 
@@ -41,7 +57,6 @@ $item = $mysql->field(array('*'))->where(array('id' => $id))->select($type);
 if (!count($item)) {
     exit(json_encode(array()));
 }
-// var_dump($item);
 
 // 模板引擎实例
 $PHPnew = new PHPnew();
